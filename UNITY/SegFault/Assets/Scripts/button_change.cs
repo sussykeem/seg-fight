39ffcad87e4764e181ac91b7279719d307909994
Randy Lin <rlin8@vols.utk.edu>
2023-04-28 16:11:40 -0400

    stuf

--- UNITY/SegFault/Assets/Scripts/button_change.cs
+++ UNITY/SegFault/Assets/Scripts/button_change.cs
@@ -4,11 +4,32 @@ using System.Collections;
 
 public class button_change : MonoBehaviour
 {
+    int counter = 0;
     public Button button_blake, button_randy, button_karim, button_turner;
 
-    public void button_press() {
-        // button_blake.GetComponent<Image>().color = Color.red;
-        GetComponent<Image>().color = Color.green;
+    public void button_press(Button button) {
+        if (counter == 0) { 
+        button.GetComponent<Image>().color = Color.red;
+            ColorBlock colorVar = button.colors;
+            colorVar.highlightedColor = new Color(0, 0, 255);
+            button_blake.colors = colorVar;
+            button_randy.colors = colorVar;
+            button_karim.colors = colorVar;
+            button_turner.colors = colorVar;
+            counter++;
+        }
+        else if (counter == 1 && button.GetComponent<Image>().color != Color.red) {
+            button.GetComponent<Image>().color = Color.blue;
+            ColorBlock colorVar = button.colors;
+            colorVar.highlightedColor = new Color(255, 255, 255);
+            button_blake.colors = colorVar;
+            button_randy.colors = colorVar;
+            button_karim.colors = colorVar;
+            button_turner.colors = colorVar;
+            counter++;
+        }
+        else
+        { }
     }
     
 }
