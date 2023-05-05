05c29623fd69b4c7d87856bd7b78bdb71b16ca24
Randy Lin <rlin8@vols.utk.edu>
2023-05-05 11:30:06 -0400

    updated charSelect, making mapSelect
    hopefully nothing breaks

--- UNITY/SegFault/Assets/Scripts/button_change.cs
+++ UNITY/SegFault/Assets/Scripts/button_change.cs
@@ -1,32 +1,40 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class button_change : MonoBehaviour
 {
     int counter = 0;
-    public Button button_blake, button_randy, button_karim, button_turner;
+    public Button button_blake, button_randy, button_karim, button_turner, back;
+    EventSystem EventSystem = EventSystem.current;
 
     public void button_press(Button button) {
         if (counter == 0) { 
         button.GetComponent<Image>().color = Color.red;
             ColorBlock colorVar = button.colors;
-            colorVar.highlightedColor = new Color(0, 0, 255);
+            //colorVar.highlightedColor = new Color(0, 0, 255);
+            colorVar.selectedColor = new Color(0, 0, 255);
             button_blake.colors = colorVar;
             button_randy.colors = colorVar;
             button_karim.colors = colorVar;
             button_turner.colors = colorVar;
             counter++;
+
         }
         else if (counter == 1 && button.GetComponent<Image>().color != Color.red) {
             button.GetComponent<Image>().color = Color.blue;
             ColorBlock colorVar = button.colors;
             colorVar.highlightedColor = new Color(255, 255, 255);
+            colorVar.selectedColor = new Color(255, 255, 255);
             button_blake.colors = colorVar;
             button_randy.colors = colorVar;
             button_karim.colors = colorVar;
             button_turner.colors = colorVar;
             counter++;
+            SceneManager.LoadScene("mapSelect");
+
         }
         else
         { }
